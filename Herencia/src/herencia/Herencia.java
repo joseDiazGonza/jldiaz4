1bf78238e3f6c1a057c582da0f5f63ca4b361bf5
JOSE LUIS DIAZ <JOSE LUIS DIAZ@PERSONAL>
2016-11-01 08:56:07 -0500

    primer cambio

---
+++ Herencia/src/herencia/Herencia.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author JOSE LUIS DIAZ
+ */
+public class Herencia {
+
+    
+    public static void main(String[] args) {
+        
+        recibe digoHola = new recibe();
+        
+        digoHola.yodigohola();
+        
+    }
+    
+}
